d3fb3eae68f1c37adee876d201e9b6f8a09ea306
unknown <banyu_putri@live.com>
2014-09-15 16:59:49 +0700

    This is my first commit ^^

---
+++ cobain.java
@@ -0,0 +1,13 @@
+public class cobain{
+	public static void main(String[] args){
+		String[][] dogs={{" Ucing", " Brown"}
+						, {" Anjing", " Black"}
+						, {" Bule", " Grey"}
+						};
+				System.out.println("Anjing saya bernama"+dogs[0][0]+" dan berwarna"+dogs[0][1]);
+				System.out.println("Anjing saya bernama"+dogs[1][0]+" dan berwarna"+dogs[1][1]);
+				System.out.println("Anjing saya bernama"+dogs[2][0]+" dan berwarna"+dogs[2][1]);
+				}
+				}
+				
+				
